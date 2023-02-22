33b4ea4fd45ce33b878e137cf4e5fcc2f33d1835
poacher <58641438+poach3r@users.noreply.github.com>
2023-02-22 17:19:51 -0500

    Add files via upload

--- autoconfig.cpp
+++ autoconfig.cpp
@@ -7,42 +7,42 @@ int main(void)
     int browser, text;
     cout << "BROWSER:\n1. Firefox\n2. Libre Wolf\n3. Surf\n";
     cin >> browser;
-    cout << "TEXT EDITOR:\n1. Vim\n2. NeoVim\n3. VSCodium\n4. VSCode";
+    cout << "TEXT EDITOR:\n1. Vim\n2. NeoVim\n3. VSCodium\n4. VSCode\n";
     cin >> text;
     system("git clone https://aur.archlinux.org/yay.git; cd ~/yay/; makepkg -si");
     if(browser==1)
     {
         if(text==1)
-            system("yay -S rofi polybar firefox i3-git neofetch thunar lxappearance vim alacritty");
+            system("yay -S lightdm rofi polybar firefox i3-git neofetch thunar lxappearance vim alacritty");
         else if(text==2)
-            system("yay -S rofi polybar firefox i3-git neofetch thunar lxappearance neovim alacritty");
+            system("yay -S lightdm rofi polybar firefox i3-git neofetch thunar lxappearance neovim alacritty");
         else if(text==3)
-            system("yay -S rofi polybar firefox i3-git neofetch thunar lxappearance vscodium-bin alacritty");
+            system("yay -S lightdm rofi polybar firefox i3-git neofetch thunar lxappearance vscodium-bin alacritty");
         else if(text==4)
-            system("yay -S rofi polybar firefox i3-git neofetch thunar lxappearance code alacritty");
+            system("yay -S lightdm rofi polybar firefox i3-git neofetch thunar lxappearance code alacritty");
     }
     else if(browser==2)
     {
         if(text==1)
-            system("yay -S rofi polybar librewolf-bin i3-git neofetch thunar lxappearance vim alacritty");
+            system("yay -S lightdm rofi polybar librewolf-bin i3-git neofetch thunar lxappearance vim alacritty");
         else if(text==2)
-            system("yay -S rofi polybar librewolf-bin i3-git neofetch thunar lxappearance neovim alacritty");
+            system("yay -S lightdm rofi polybar librewolf-bin i3-git neofetch thunar lxappearance neovim alacritty");
         else if(text==3)
-            system("yay -S rofi polybar librewolf-bin i3-git neofetch thunar lxappearance vscodium-bin alacritty");
+            system("yay -S lightdm rofi polybar librewolf-bin i3-git neofetch thunar lxappearance vscodium-bin alacritty");
         else if(text==4)
-            system("yay -S rofi polybar librewolf-bin i3-git neofetch thunar lxappearance code alacritty");
+            system("yay -S lightdm rofi polybar librewolf-bin i3-git neofetch thunar lxappearance code alacritty");
     }
     else if(browser==3)
     {
         if(text==1)
-            system("yay -S rofi polybar surf i3-git neofetch thunar lxappearance vim alacritty");
+            system("yay -S lightdm rofi polybar surf i3-git neofetch thunar lxappearance vim alacritty");
         else if(text==2)
-            system("yay -S rofi polybar surf i3-git neofetch thunar lxappearance neovim alacritty");
+            system("yay -S lightdm rofi polybar surf i3-git neofetch thunar lxappearance neovim alacritty");
         else if(text==3)
-            system("yay -S rofi polybar surf i3-git neofetch thunar lxappearance vscodium-bin alacritty");
+            system("yay -S lightdm rofi polybar surf i3-git neofetch thunar lxappearance vscodium-bin alacritty");
         else if(text==4)
-            system("yay -S rofi polybar surf i3-git neofetch thunar lxappearance code alacritty");
+            system("yay -S lightdm rofi polybar surf i3-git neofetch thunar lxappearance code alacritty");
     }
-    printf("FINISHED INSTALLING PACKAGES");
-    system("neofetch");
+    printf("FINISHED INSTALLING PACKAGES\n");
+    system("sudo systemctl enable lightdm; sudo systemctl start lightdm");
 }
