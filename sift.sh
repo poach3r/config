49fcc86871268e9a477728f47af6380f7cc3a230
poacher <58641438+poach3r@users.noreply.github.com>
2023-02-28 11:48:10 -0500

    Add files via upload

--- sift.sh
+++ sift.sh
@@ -1,12 +1,9 @@
 #!/bin/bash
 
-xbps=$(xbps-install $1)
-
-if [ "$xbps" == "Package '$1' not found in repository pool." ]
-then
-    first=$(echo $1 | head -c 1)
-    wget http://ftp.us.debian.org/debian/pool/main/$first/$1/$2.deb
-    xdeb -Sde ~/$2.deb
-fi
+first=$(echo $1 | head -c 1)
+wget http://ftp.us.debian.org/debian/pool/main/$first/$1/$2.deb
+xdeb -Sde ~/$2.deb
+rm -rf ~/$2.deb
+xbps-install -R binpkgs $2
 
 echo "Done!"
