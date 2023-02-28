9fee57d78b66a50c94b934a34ec29999e2929ef5
poacher <58641438+poach3r@users.noreply.github.com>
2023-02-28 13:09:47 -0500

    Add files via upload

--- sift.sh
+++ sift.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 first=$(echo $1 | head -c 1)
-wget http://ftp.us.debian.org/debian/pool/main/$first/$1/$2.deb
-xdeb -Sde ~/$2.deb
-rm -rf ~/$2.deb
+wget -O /tmp/package.deb http://ftp.us.debian.org/debian/pool/main/$first/$1/$2.deb
+xdeb -Sde /tmp/package.deb
+rm /tmp/package.deb
 xbps-install -R binpkgs $2
 
 echo "Done!"
